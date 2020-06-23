eb916ff3c062507471cdb1f2965c93cd85bc95a7
Nurislam Ab <you@example.com>
2020-06-23 20:45:48 +0500

    Migrate the post table

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
 
-  respurces :posts[:new, :create, :index]
+  resources :posts[:new, :create, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

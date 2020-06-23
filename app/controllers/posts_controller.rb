020be16085a55e44c5b3ccee83e6b573eda5c905
Nurislam Ab <you@example.com>
2020-06-24 00:52:15 +0500

    Minor fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,37 +1,35 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[show edit update destroy]
-  before_action :authenticate_user!, except: %i[index show]
+    before_action :authenticate_user!, only: %i[new create]
 
-  def new
-    @post = current_user.posts.build
-  end
-
-  def create
-    @post = current_user.posts.build(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to root_path, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    def new
+        @post = current_user.posts.build
     end
-  end
 
-  def index
-    @posts = Post.all
-    @post = Post.new
-  end
+    def create
+        @post = current_user.posts.build(post_params)
 
-  private
+        respond_to do |format|
+            if @post.save
+                format.html { redirect_to root_path, notice: 'Post was successfully created.' }
+                format.json { render :show, status: :created, location: @post }
+            else
+                format.html { render :new }
+                format.json { render json: @post.errors, status: :unprocessable_entity }
+            end
+        end
+    end
 
-  def post_params
-    params.require(:post).permit(:post_body)
-  end
+    def index
+        @posts = Post.all
+        @post = Post.new
+    end
 
-  def set_post
-    @post = Post.find(params[:id])
-  end
+    private
+    def post_params
+        params.require(:post).permit(:post_body)
+    end
+
+    def set_post
+        @post = Post.find(params[:id])
+    end
 end

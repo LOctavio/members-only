0ac950829159b860ecbe74d388d909b17775234a
Nurislam Ab <you@example.com>
2020-06-23 21:13:34 +0500

    Add simple index view and change index action in the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,10 @@ before_action :authenticate_user!, except: [:index, :show]
         @post = Post.new
     end
 
+    def index
+        @posts = Post.all
+    end
+
     private
     def post_params
         params.require(:post).permit(:post_body)
